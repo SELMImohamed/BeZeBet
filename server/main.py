a4b989adf56c710e21db82cd448db290d8e48501
Bulbizar <adrienesteves@icloud.com>
2023-03-27 15:46:32 +0200

    database link

--- server/main.py
+++ server/main.py
@@ -1,6 +1,25 @@
 from fastapi import FastAPI
+import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = FastAPI()
+
+database_connection = mysql.connector.connect(
+    host=os.getenv("host"),
+    port=os.getenv("port"),
+    user=os.getenv("user"),
+    password=os.getenv("password"),
+    database=os.getenv("database")
+)
+
+print(database_connection)
+
+mycursor = database_connection.cursor()
+
+mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
 
 
 @app.get("/")
@@ -8,6 +27,6 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/register")
+async def register():
+    return {"message": ""}
