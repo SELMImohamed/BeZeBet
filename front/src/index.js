33724a22086c69531c4336921d88be75e0640131
AxelPerrin <91256042+AxelPerrin@users.noreply.github.com>
2023-03-27 11:53:43 +0200

    set route

--- front/src/index.js
+++ front/src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+import Root from "./routes/Routes";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+     <RouterProvider router={router} />
     <App />
   </React.StrictMode>
 );
