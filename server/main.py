bb05eda4945d5d3f7b6cecb326e855ad14f1a80c
SELMImohamed <91205436+SELMImohamed@users.noreply.github.com>
2023-03-28 11:36:24 +0200

    login and register api call

--- server/main.py
+++ server/main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import mysql.connector
 from dotenv import load_dotenv
+from pydantic import BaseModel
+from datetime import date, datetime, time, timedelta
 import os
 
 load_dotenv()
@@ -17,12 +19,36 @@ database_connection = mysql.connector.connect(
 
 print(database_connection)
 
+class User(BaseModel):
+    name: str
+    email: str
+    password: str
+    date : date
+    coins : int
+    nbParis : int
+    nbParisWin : int
 
+class UserLog(BaseModel):
+    email: str
+    password: str
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/register")
-async def register():
-    return {"message": ""}
+@app.post("/register")
+async def register(user: User):
+    database_connection.cursor().execute("INSERT INTO user (name, email, password, date, coins, nbParis, nbParisWin) VALUES (%s, %s, %s, %s, %s, %s, %s)", (user.name, user.email, user.password, user.date, user.coins, user.nbParis, user.nbParisWin))
+    database_connection.commit()
+    database_connection.close()
+    return {"message": "registered"}
+
+@app.post("/login")
+async def login(userlog: UserLog):
+    database_connection.cursor().execute("SELECT * FROM user WHERE email = ? AND password = ?", (userlog.email, userlog.password))
+    result = database_connection.cursor().fetchone()
+    database_connection.commit()
+    database_connection.close()
+    if result is None:
+        raise  HTTPException(status_code=400, detail="invalid email or password")
+    return {"message": "logged in"}
