f46cb87772881c55e945e5d0b468185af78602da
Bulbizar <adrienesteves@icloud.com>
2023-03-31 09:40:39 +0200

    modify Home page

--- front/src/routes/Home.js
+++ front/src/routes/Home.js
@@ -1,5 +1,5 @@
 import React , {useEffect} from 'react';
-import { Link } from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import "../styles/NavBar.css";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
@@ -24,17 +24,18 @@ export default function Home() {
       </style>
 
       <Box align="center">
-        <h1 className="titre">BE ZEBET</h1>
-        <h3>Paris entre amis ?</h3>
+        <h1 className="titre"><span>BE</span> ZEBET</h1>
+        <h3>Créer ton Paris entre amis ?</h3>
         <Button
           sx={{
             borderRadius: 2,
             p: 2,
             minWidth: 300,
-            color: "red",
+            color: "#FBCF0A",
           }}
-          color="error"
+          color="inherit"
           variant="outlined"
+          onClick={}
         >
           Get Start
         </Button>
